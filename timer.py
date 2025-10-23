8ec526a7ce7ea7fcebd5e7e2ccd21460c917c729
habibmirsyad <habmirs@gmail.com>
2025-10-23 10:52:20 +0700

    basic GUI

--- timer.py
+++ timer.py
@@ -1,17 +1,47 @@
 import time
 import winsound
+from tkinter import *
 
+root = Tk()
+root.title("Remind Me After")
 
-myTime = int(input("Enter the time in second: "))
-task = input("Enter task to do: ").upper()
-for i in range(int(myTime), 0, -1):
-    seconds = i % 60
-    mins = int(i / 60) % 60
-    hours = int(i / 3600)
-    print(f"{hours:02}:{mins:02}:{seconds:02}")
-    time.sleep(1)
+frame1 = Frame(root)
+frame1.pack()
+myTimeLabel = Label(frame1, text="Enter the time in second: ", width=20, anchor="e")
+myTimeInput = Entry(frame1)
+myTimeLabel.pack(side="left")
+myTimeInput.pack(side="left")
 
-print(f"IT'S TIME TO {task} NOW!!!")
-winsound.PlaySound('alarm-clock.wav', winsound.SND_FILENAME)
+frame2 = Frame(root)
+frame2.pack()
+taskLabel = Label(frame2, text="Enter task to do: ",width=20, anchor="e")
+taskInput = Entry(frame2)
+taskLabel.pack(side="left")
+taskInput.pack(side="left")
 
-# Buat GUI
+# Label for countdown and task
+countdownLabel = Label(root, text="", font=(20))
+timesUpLabel = Label(root, text="")
+countdownLabel.pack()
+timesUpLabel.pack()
+
+def Timer():
+    myTime = int(myTimeInput.get())
+    task = taskInput.get().upper()
+
+    for i in range(myTime, 0, -1):
+        seconds = i % 60
+        mins = int(i / 60) % 60
+        hours = int(i / 3600)
+        
+        countdownLabel.config(text=f"{hours:02}:{mins:02}:{seconds:02}")
+        root.update()
+        time.sleep(1)
+
+    timesUpLabel.config(text=f"IT'S TIME TO {task} NOW!!!")
+    root.update()
+    # winsound.PlaySound('alarm-clock.wav', winsound.SND_FILENAME)
+
+button0 = Button(root, text="OK", command=Timer).pack()
+
+root.mainloop()
