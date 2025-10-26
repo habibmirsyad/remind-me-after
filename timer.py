633e516fe77e5bdb3018e75f88d20b9aa72daae7
habibmirsyad <habmirs@gmail.com>
2025-10-26 21:05:50 +0700

    make user input only number in timer and timer greater then 0

--- timer.py
+++ timer.py
@@ -6,22 +6,34 @@ from tkinter import ttk
 root = Tk()
 root.title("Remind Me After")
 
+def OnlyNum(timeInput):
+    return timeInput.isdigit() or timeInput == ""
+
+validate = root.register(OnlyNum)
+
 def Timer():
     myTime = int(myTimeInputH.get())*3600 + int(myTimeInputMin.get())*60 + int(myTimeInputSec.get())
     task = taskInput.get().upper()
 
-    for i in range(myTime, -1, -1):
-        seconds = i % 60
-        mins = int(i / 60) % 60
-        hours = int(i / 3600)
-        
-        countdownLabel.config(text=f"{hours:02}:{mins:02}:{seconds:02}")
-        root.update()
-        time.sleep(1)
+    if myTime <= 0:
+        timesUpLabel.config(text="Timer must be greater than 0!")
+    else:
+        timesUpLabel.config(text="")
+        for i in range(myTime, -1, -1):
+            seconds = i % 60
+            mins = int(i / 60) % 60
+            hours = int(i / 3600)
+            
+            countdownLabel.config(text=f"{hours:02}:{mins:02}:{seconds:02}")
+            root.update()
+            time.sleep(1)
 
-    timesUpLabel.config(text=f"IT'S TIME TO {task} NOW!!!")
-    root.update()
-    # winsound.PlaySound('alarm-clock.wav', winsound.SND_FILENAME)
+        if task == "":
+            timesUpLabel.config(text="TIME'S UP!!!")
+        else:
+            timesUpLabel.config(text=f"IT'S TIME TO {task} NOW!!!")
+        root.update()
+        # winsound.PlaySound('alarm-clock.wav', winsound.SND_FILENAME)
 
 
 varSec = [f"{i:02d}" for i in range(60)]
@@ -31,11 +43,11 @@ varH = [f"{i:02d}" for i in range(24)]
 frame1 = Frame(root)
 frame1.pack()
 myTimeLabel = Label(frame1, text="Enter the time (HH:MM:SS)", width=25, anchor="e")
-myTimeInputH = ttk.Combobox(frame1, values=varH, width=2)
+myTimeInputH = ttk.Combobox(frame1, values=varH, width=2, validate="key", validatecommand=(validate, "%P"))
 myTimeInputH.current(0)
-myTimeInputMin = ttk.Combobox(frame1, values=varMin, width=2)
+myTimeInputMin = ttk.Combobox(frame1, values=varMin, width=2, validate="key", validatecommand=(validate, "%P"))
 myTimeInputMin.current(0)
-myTimeInputSec = ttk.Combobox(frame1, values=varSec, width=2)
+myTimeInputSec = ttk.Combobox(frame1, values=varSec, width=2, validate="key", validatecommand=(validate, "%P"))
 myTimeInputSec.current(0)
 
 myTimeLabel.pack(side="left")
